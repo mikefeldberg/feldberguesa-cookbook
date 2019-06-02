f670d379c8d660cbcc36ba5f1227c52dccd1d1ce
Mike Feldberg <mikefeldberg@gmail.com>
2019-06-01 23:58:55 -0700

    add auth screener to recipe new and edit routes; add dead recipe link redirect

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -16,9 +16,9 @@ module.exports = {
 };
 
 function index(req, res) {
-    User.find({}).exec(function(err, users){
-        Recipe.find({}).exec(function(err, recipes) {
-            Favorite.find({}).exec(function(err, favorites) {
+    User.find({}).exec(function (err, users) {
+        Recipe.find({}).exec(function (err, recipes) {
+            Favorite.find({}).exec(function (err, favorites) {
                 res.render('recipes/index', { recipes, users, favorites, sessionUser: req.user });
             });
         });
@@ -26,52 +26,60 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    Recipe.findById(req.params.id).exec(function(err, recipe) {
-        Comment.find({ recipeId: recipe._id, deletedAt: null }).exec(function(err, comments) {
-            User.find({}).exec(function(err, users) {    
-                User.findById(recipe.userId).exec(function(err, recipeAuthor) {
-                    Favorite.find({ recipeId: recipe._id, deletedAt: null }).exec(function(err, favorites) {
+    Recipe.findById(req.params.id).exec(function (err, recipe) {
+        if (!recipe) {
+            res.redirect('/recipes');
+        } else {
+            Comment.find({ recipeId: recipe._id, deletedAt: null }).exec(function (err, comments) {
+                User.find({}).exec(function (err, users) {
+                    User.findById(recipe.userId).exec(function (err, recipeAuthor) {
+                        Favorite.find({ recipeId: recipe._id, deletedAt: null }).exec(function (err, favorites) {
 
-                        function getFormattedDate(unparsedDate) {
-                            var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-                            var monthNumber = unparsedDate.getMonth();
-                            var month = months[monthNumber];
-                            var day = unparsedDate.getDate();
-                            var year = unparsedDate.getFullYear();
-                            var displayDate = month + " " + day + ", " + year
-                            return displayDate;
-                        }
+                            function getFormattedDate(unparsedDate) {
+                                var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+                                var monthNumber = unparsedDate.getMonth();
+                                var month = months[monthNumber];
+                                var day = unparsedDate.getDate();
+                                var year = unparsedDate.getFullYear();
+                                var displayDate = month + " " + day + ", " + year
+                                return displayDate;
+                            }
 
-                        var dateCreated = getFormattedDate(recipe.createdAt);
-                        var dateUpdated = null;
+                            var dateCreated = getFormattedDate(recipe.createdAt);
+                            var dateUpdated = null;
 
-                        if (recipe.updatedAt) {
-                            var dateUpdated = getFormattedDate(recipe.updatedAt);
-                        }
+                            if (recipe.updatedAt) {
+                                var dateUpdated = getFormattedDate(recipe.updatedAt);
+                            }
 
-                        var favoriteCount = favorites.length;
-                        var isFavorited = false;
-                        if (req.user) {
-                            Favorite.findOne({ userId: req.user._id, recipeId: req.params.id, deletedAt: null }, function(err, favorite) {
-                                res.render('recipes/show', { recipe, users, recipeAuthor, dateCreated, dateUpdated, sessionUser: req.user, comments, favoriteCount, isFavorited: !!favorite,  });
-                            });
-                        } else {
-                            res.render('recipes/show', { recipe, users, recipeAuthor, dateCreated, dateUpdated, sessionUser: null, comments, favoriteCount, isFavorited,  });
-                        }
+                            var favoriteCount = favorites.length;
+                            var isFavorited = false;
+                            if (req.user) {
+                                Favorite.findOne({ userId: req.user._id, recipeId: req.params.id, deletedAt: null }, function (err, favorite) {
+                                    res.render('recipes/show', { recipe, users, recipeAuthor, dateCreated, dateUpdated, sessionUser: req.user, comments, favoriteCount, isFavorited: !!favorite, });
+                                });
+                            } else {
+                                res.render('recipes/show', { recipe, users, recipeAuthor, dateCreated, dateUpdated, sessionUser: null, comments, favoriteCount, isFavorited, });
+                            }
+                        });
                     });
                 });
             });
-        });
+        }
     });
 }
 
 function newRecipe(req, res) {
-    res.render('recipes/new', { sessionUser: req.user });
+    if (!req.user) {
+        res.redirect('/recipes');
+    } else {
+        res.render('recipes/new', {sessionUser: req.user});
+    }
 }
 
 function create(req, res) {
     var recipe = new Recipe;
-    
+
     var ingredientQty = req.body.qty;
     var ingredientName = req.body.ingredient;
     var ingredientPrep = req.body.preparation;
@@ -83,7 +91,7 @@ function create(req, res) {
                 qty: ingredientQty[i],
                 name: ingredientName[i],
                 prep: ingredientPrep[i],
-            };   
+            };
 
             if (ingredient.qty) {
                 ingredients.push(ingredient)
@@ -103,15 +111,15 @@ function create(req, res) {
     }
 
     recipe.ingredients = ingredients;
-    
+
     recipe.name = req.body.name;
     recipe.description = req.body.description;
     if (req.body.instructions.length > 1) {
         for (var i = 0; i < req.body.instructions.length; i++) {
-           if (req.body.instructions[i]) {recipe.instructions.push(req.body.instructions[i])};
+            if (req.body.instructions[i]) { recipe.instructions.push(req.body.instructions[i]) };
         }
     } else {
-        if (req.body.instructions) {recipe.instructions.push(req.body.instructions)};
+        if (req.body.instructions) { recipe.instructions.push(req.body.instructions) };
     }
     recipe.skillLevel = req.body.skillLevel;
     recipe.timePrep = req.body.timePrep;
@@ -122,16 +130,24 @@ function create(req, res) {
     recipe.imageUrl = req.body.imageUrl;
     recipe.addedBy = req.user.name;
     recipe.userId = req.user._id;
-    
+
     recipe.save(function (err) {
         res.redirect('/recipes');
     });
 }
 
 function edit(req, res) {
-    Recipe.findById(req.params.id).exec(function (err, recipe) {
-        res.render('recipes/edit', { recipe, sessionUser: req.user });
-    });
+    if (!req.user) {
+        res.redirect('/recipes');
+    } else {
+        Recipe.findById(req.params.id).exec(function (err, recipe) {
+            if (req.user._id == recipe.userId) {
+                res.render('recipes/edit', { recipe, sessionUser: req.user });
+            } else {
+                res.redirect('/recipes');
+            }
+        });
+    }
 }
 
 function update(req, res) {
@@ -141,14 +157,14 @@ function update(req, res) {
         var ingredientName = req.body.ingredient;
         var ingredientPrep = req.body.preparation;
         var ingredients = [];
-        
+
         if (ingredientQty.length > 1) {
             for (var i = 0; i < ingredientQty.length; i++) {
                 var ingredient = {
                     qty: ingredientQty[i],
                     name: ingredientName[i],
                     prep: ingredientPrep[i],
-                };   
+                };
 
                 if (ingredient.qty) {
                     ingredients.push(ingredient)
@@ -175,10 +191,10 @@ function update(req, res) {
 
         if (req.body.instructions.length > 1) {
             for (var i = 0; i < req.body.instructions.length; i++) {
-               if (req.body.instructions[i]) {recipe.instructions.push(req.body.instructions[i])};
+                if (req.body.instructions[i]) { recipe.instructions.push(req.body.instructions[i]) };
             }
         } else {
-            if (req.body.instructions) {recipe.instructions.push(req.body.instructions)};
+            if (req.body.instructions) { recipe.instructions.push(req.body.instructions) };
         }
 
         recipe.skillLevel = req.body.skillLevel;
