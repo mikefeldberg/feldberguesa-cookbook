0ca303357518c2c850f854cd5b29100dfe7a640a
Mike Feldberg <mikefeldberg@gmail.com>
2019-06-02 21:38:32 -0700

    add favicon; change oauth redirect to front page

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,14 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
     'google',
     {
-        successRedirect: `back`,
+        successRedirect: '/',
         failureRedirect: '/'
     }
 ));
 
 router.get('/logout', function(req, res) {
     req.logout();
-    res.redirect(`back`);
+    res.redirect('/');
 });
 
 module.exports = router;
