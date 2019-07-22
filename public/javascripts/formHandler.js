a1c915ae9fa860119020b330c334d24505959774
Mike Feldberg <mikefeldberg@gmail.com>
2019-07-21 19:34:16 -0700

    fix add and edit page row addition & deletion (#11)

--- public/javascripts/formHandler.js
+++ public/javascripts/formHandler.js
@@ -1,32 +1,87 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#newRecipeForm").keydown(function (event) {
         if (event.keyCode == 13) {
             event.preventDefault();
+            // $(this).next().focus();
             return false;
         }
     });
 });
 
-$(".prep").keydown(function(event) {
-    if (event.keyCode == 13) {
-        addIngredientRow();
-        return false;
-    }
-})
-
-$(".instruction").keydown(function(event) {
-    if (event.keyCode == 13) {
-        addInstructionRow();
-        return false;
-    }
-})
-
-$(".qty, .name").keydown(function(event) {
-    if (event.keyCode == 13) {
-        $(this).next('input').focus();
-        return false;
-    }
+$(document).ready(function () {
+    $("#editRecipeForm").keydown(function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            // $(this).next().focus();
+            return false;
+        }
+    });
 });
+
+// $(document).ready(function () {
+//     $('.ingredientInput').bind('keypress', function (event) {
+//         if (event.which === 13) {
+//             console.log('whaaaaaat')
+//             var nextItem = $(this).next('.ingredientInput');
+//             console.log(nextItem)
+//             nextItem.focus();
+//         }
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.instructionInput').bind('keypress', function (event) {
+//         if (event.which === 13) {
+//             console.log('whaaaaaat')
+//             var nextItem = $(this).next('.instructionInput');
+//             nextItem.focus();
+//         }
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.addlInfoInput').bind('keypress', function (event) {
+//         if (event.which === 13) {
+//             console.log('whaaaaaat')
+//             var nextItem = $(this).next('.addlInfoInput');
+//             nextItem.focus();
+//         }
+//     });
+// });
+
+
+// $(document).ready(function () {
+//     $("#editRecipeForm").keydown(function (event) {
+//         if (event.keyCode == 13) {
+//             // event.preventDefault();
+//             $(this).next().focus();
+//             // return false;
+//         }
+//     });
+// });
+
+
+
+// $(".prep").keydown(function(event) {
+//     if (event.keyCode == 13) {
+//         addIngredientRow();
+//         return false;
+//     }
+// })
+
+// $(".instruction").keydown(function(event) {
+//     if (event.keyCode == 13) {
+//         addInstructionRow();
+//         return false;
+//     }
+// })
+
+// $(".qty, .name").keydown(function(event) {
+//     if (event.keyCode == 13) {
+//         $(this).next('input').focus();
+//         return false;
+//     }
+// });
 
 function addIngredientRow() {
     $(".ingredientRow > p:first-child").clone(true).insertBefore(".ingredientRow > p:last-child").find(":text").val("");
@@ -37,42 +92,52 @@ function addInstructionRow() {
     return false;
 }
 
-$(".removeIngredient").click(function() {
+$(".removeIngredient").click(function () {
     $(this).parent().remove();
     return false;
 });
 
-$(".removeInstruction").click(function() {
+$(".removeInstruction").click(function () {
     $(this).parent().remove();
     return false;
 });
 
 
-$(".addEditIngredient").click(function() {
+// $(".removeEditIngredient").click(function() {
+//     $(this).parent().remove();
+// });
+
+$(".addNewIngredient").click(function () {
     $(".ingredientRow > p:last-child").clone(true).insertBefore(".ingredientRow > p:last-child").find(":text").val("");
     return false;
 });
 
-$(".removeEditIngredient").click(function() {
-    $(this).parent().remove();
-});
-
-$(".addEditInstruction").click(function() {
+$(".addNewInstruction").click(function () {
     $(".instructionRow > p:last-child").clone(true).insertBefore(".instructionRow > p:last-child").find(":text").val("");
     return false;
 });
 
-$(".removeEditInstruction").click(function() {
-    $(this).parent().remove();
+$(".addEditIngredient").click(function () {
+    $(".ingredientRow > p:last-child").clone(true).insertBefore(".ingredientRow > p:last-child").find(":text").val("");
     return false;
 });
 
-$(".openDeleteRecipeDialogue").click(function() {
+$(".addEditInstruction").click(function () {
+    $(".instructionRow > p:last-child").clone(true).insertBefore(".instructionRow > p:last-child").find(":text").val("");
+    return false;
+});
+
+// $(".removeEditInstruction").click(function() {
+//     $(this).parent().remove();
+//     return false;
+// });
+
+$(".openDeleteRecipeDialogue").click(function () {
     $(this).attr("disabled", "disabled"); /* This is good */
     $('.deleteRecipeDialogueContainer').removeAttr("hidden");
 });
 
-$(".deleteRecipeCancel").click(function() {
+$(".deleteRecipeCancel").click(function () {
     $('.deleteRecipeDialogueContainer').attr("hidden", "");
     $('.openDeleteRecipeDialogue').removeAttr("disabled"); /* This is good */
 });
@@ -82,30 +147,17 @@ $(".deleteRecipeCancel").click(function() {
 //     $('.deleteCommentDialogueContainer').removeAttr("hidden"); 
 // });
 
-$(".deleteCommentCancel").click(function() {
+$(".deleteCommentCancel").click(function () {
     $('.deleteCommentDialogueContainer').attr("hidden", "");
     $('.openDeleteCommentDialogue').removeAttr("disabled"); /* This is good */
 });
 
-$(".openDeleteCommentDialogue").click(function() {
+$(".openDeleteCommentDialogue").click(function () {
     $(this).attr("disabled", "disabled");
     $(this).parent().children().removeAttr("hidden");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(".deleteCommentCancel").click(function() {
+$(".deleteCommentCancel").click(function () {
     $('.deleteCommentConfirm').attr("hidden", "");
     $('.deleteComment').removeAttr("hidden");
 });
